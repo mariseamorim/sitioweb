98ef090eb18b57fd5a93c6139bf5a4458a2daaed
Marise Amorim <marise.amorim@corteva.com>
2026-02-25 18:46:59 -0300

    Add Prisma Accelerate extension for Vercel serverless compatibility

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+const globalForPrisma = global as unknown as { prisma: ReturnType<typeof makePrismaClient> }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  })
+function makePrismaClient() {
+  return new PrismaClient().$extends(withAccelerate())
+}
+
+export const prisma = globalForPrisma.prisma ?? makePrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
